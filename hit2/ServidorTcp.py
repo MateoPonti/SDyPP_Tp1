8e099328d3974827b32212da2add8cb0f59a71aa
jbrodi99 <juancruz.99.jcr@gmail.com>
2026-08-23 19:51:41 -0300

    ajuste de B en hit2

--- hit2/ServidorTcp.py
+++ hit2/ServidorTcp.py
@@ -9,22 +9,13 @@ server_socket.listen(1)
 
 print(f"Servidor B esperando conexiones en el puerto {PORT}...")
 
+conn, addr = server_socket.accept()
+print(f"A se conectó desde {addr}")
+
 while True:
-    conn, addr = server_socket.accept()
-    print(f"A se conectó desde {addr}")
 
-    try:
-        while True:
-            mensaje = conn.recv(1024)
-            if not mensaje:
-                print("A cerró la conexión")
-                break
+    mensaje = conn.recv(1024).decode()
+    print(f"Saludo recibido: {mensaje}")
 
-            print(f"Saludo recibido: {mensaje.decode()}")
-
-            respuesta = "Hola A, soy B"
-            conn.sendall(respuesta.encode())
-    except (ConnectionResetError, BrokenPipeError, OSError) as e:
-        print(f"Conexión con A perdida ({e})")
-    finally:
-        conn.close()
+    respuesta = "Hola A, soy B"
+    conn.sendall(respuesta.encode())
